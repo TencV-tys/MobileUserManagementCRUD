466a7b4c637bd62aa6e6219aac79734969d901a0
Vincent <vincenttayros078@gmail.com>
2026-06-10 15:18:52 +0800

    aded for now

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,10 +3,36 @@ import { type RootStack } from "../navigator/Navigator";
 import { View, TouchableOpacity, Text, Alert, StyleSheet, FlatList } from 'react-native'
 import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-
+import { User } from '../database/userData'
+import UserDatabase from '../database/userData'
 type HomeScreenNavigationProp = NativeStackNavigationProp<RootStack, 'Home'>;
 
 const HomeScreen = () => {
+  const navigation = useNavigation<HomeScreenNavigationProp>();
+  const [users, setUsers] = useState<User[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
+
+  useFocusEffect(
+    useCallback(() => {
+      if (UserDatabase.isReady()) loadUsers()
+    }, [])
+  )
+
+  const loadUsers = async (): Promise<void> => {
+    try {
+      setLoading(true)
+      const allUsers = await UserDatabase.getAllUser()
+      setUsers(allUsers)
+    } catch (e) {
+      console.error('Error loading', e)
+      Alert.alert('Error , Failed to load users')
+    } finally {
+      setLoading(false)
+    }
+  }
+
+
+
   return (
     <View>
       <Text>Home Screen</Text>
